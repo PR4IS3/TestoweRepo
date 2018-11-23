ed27ee2ec282b24e5a1106294bf096e8e4365f36
PR4IS3 <43931741+PR4IS3@users.noreply.github.com>
2018-11-23 17:19:06 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,29 @@
+create database Zadanie
+GO
+
+use Zadanie
+GO
+
+CREATE TABLE [OLE DB Destination] (
+    [X] varchar(50),
+    [Y] varchar(50),
+    [month] varchar(50),
+    [day] varchar(50),
+    [FFMC] varchar(50),
+    [DMC] varchar(50),
+    [DC] varchar(50),
+    [ISI] varchar(50),
+    [temp] varchar(50),
+    [RH] varchar(50),
+    [wind] varchar(50),
+    [rain] varchar(50),
+    [area] varchar(50)
+)
+
+CREATE TABLE [OLE DB Destination2] (
+    [variance of Wavelet Transformed image] varchar(50),
+    [skewness of Wavelet Transformed image] varchar(50),
+    [curtosis of Wavelet Transformed image] varchar(50),
+    [entropy of image] varchar(50),
+    [class] varchar(50)
+)
